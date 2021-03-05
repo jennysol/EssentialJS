d9da3c648237a3cb256615df7b796532fb2cb832
jennysol <jennyssoliver@gmail.com>
2021-03-05 20:35:17 -0300

    Understanding reduce root

---
+++ Array/Reduce/Reduce3.js
@@ -0,0 +1,12 @@
+Array.prototype.reduce2 = function(callback, initialValue) {
+    const initialIndex = initialValue ? 0 : 1
+    let accumulator = initialValue ||  this[0]
+    for (let i = initialIndex; i < this.length; i++) {
+        accumulator = callback(accumulator, this[i], i, this)
+    }
+    return accumulator
+}
+
+const sum = (total, value) => total + value
+const nums = [1, 2, 3, 4, 5, 6]
+console.log(nums.reduce2(sum, 21))
